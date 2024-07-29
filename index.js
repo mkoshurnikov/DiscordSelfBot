f1c79771fd9a8c59cea9369214869dd65262e236
mkoshurnikov <q0utel1@gmail.com>
2024-07-29 18:32:46 +0300

    add variables

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-require('dotenv').config()
+require('dotenv').config();
+const map = require('./variables');
 
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client({checkUpdate: false});
 
 client.on('ready', async () => {
   console.log('Discord PLUS is ready!');
-  client.channels.cache.get('1249406942254727190').send('Start...');
+  client.channels.cache.get(map.defaultChannel).send('Start...');
 });
 
 //anti-monkey variable
 let monkey = 3;
 
-//dngr
+//dangero
 client.on("messageCreate", message => {
-    if (message.author.id == '335105483662819328' && message.channelId == '1236328199298748416' 
-      && message.content.startsWith('<@&1236305228794433648') && message.content.includes('+') && monkey > 0){
-      client.channels.cache.get('1236328199298748416').send('+');
+  if (message.author.id == map.dangeroId && message.channelId == map.gienaChannelId 
+    && message.content.startsWith(map.mentionStart + map.gienaRole) && message.content.includes('+') && monkey > 0){
+      client.channels.cache.get(map.gienaChannelId).send('+');
       monkey--;
 }});
 
-//stck
+//stick
 client.on("messageCreate", message => {
-  if (message.author.id == '322895366221463573' && message.channelId == '1236328199298748416' 
-    && message.content.startsWith('<@&1236305228794433648') && message.content.includes('+') && monkey > 0){
-    client.channels.cache.get('1236328199298748416').send('+');
-    monkey--;
+  if (message.author.id == map.stickId && message.channelId == map.gienaChannelId 
+    && message.content.startsWith(map.mentionStart + map.gienaRole) && message.content.includes('+') && monkey > 0){
+      client.channels.cache.get(map.gienaChannelId).send('+');
+      monkey--;
 }});
 
 client.login(process.env.TOKEN);
---
+++ variables.js
@@ -0,0 +1,10 @@
+const map = {
+    mentionStart: "<@&", 
+    dangeroId: "335105483662819328",
+    stickId: "322895366221463573",
+    gienaChannelId: "1236328199298748416",
+    defaultChannel: "1249406942254727190",
+    gienaRole: "1236305228794433648"
+}
+
+module.exports = map;
