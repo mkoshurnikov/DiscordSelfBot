70d072309f761be802b350d7b09d6edeaa4b2ed8
mkoshurnikov <q0utel1@gmail.com>
2024-12-21 18:07:18 +0200

    add comments

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.on('ready', async () => {
   client.channels.cache.get(map.startChannel).send('Start...');
 });
 
-//anti-monkey variable
+//anti-spam variable
 let monkey = 2;
 
 //begin searching at position 21, skip mention id <@...>(22 chars total)
@@ -21,7 +21,7 @@ client.on("messageCreate", message => {
           if (message.content.includes('+', searchPosition) || message.content.includes('1', searchPosition)){
             setTimeout(()=> {
               client.channels.cache.get(map.gienaChannelId).send('+')
-              },1000 * 0.33);
+              },1000 * 0.33); //delay in ms
             monkey--;
           }
       }
