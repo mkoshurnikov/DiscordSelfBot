aa6e1c1a3cc6a3276701a097d06676ee58437ff4
Max <mkosurnikov1@gmail.com>
2026-02-28 14:54:54 +0200

    v2 petr bot

--- index.js
+++ index.js
@@ -11,18 +11,42 @@ client.on('ready', () => {
     .get(map.startChannel)
     ?.send('Start...');
 
-  let button1Id = 'raid_melee';
+  let antiSpam = 2;
   
-  // click 1st button
-  client.channels.fetch('1354880760917528787')
-    .then(channel => channel.messages.fetch('1476926871705878625'))
-    .then(message => {
+  client.on("messageCreate", message => {
+    if (!map.usersId.includes(message.author.id)) return;
+    if (message.channelId != map.bratosikChannelId) return;
+    if (!message.content.startsWith(map.mentionStart + map.raidRole)) return;
+    if (antiSpam > 2) return;
+    if (message.content.includes('1', map.searchPosition) || message.content.includes('2', map.searchPosition)){
       setTimeout(()=> {
-        return message.clickButton('raid_melee');
-      },1000 * 2); //delay, sec
-    })
-    .then(() => console.log('Clicked melee'))
-    .catch(console.error);
-});
+        message.react('✅')
+      },1000 * 10); //delay, sec
+      antiSpam++;
+    }
+  });
 
-client.login(process.env.TOKEN);  
+  client.on("messageCreate", message => {
+    if (message.author.id != map.botId) return;
+    if (message.channelId != map.bratosikChannelId) return;
+    if (!message.content.startsWith(map.mentionStart + map.raidRole)) return;
+    setTimeout(()=> {
+      message.clickButton(map.meleeButtonId)
+        .then(() => console.log("Clicked"))
+        .catch(console.error);
+      },1000 * 2); //delay, sec
+  });
+
+//   // click 1st button
+//     client.channels.fetch('1354880760917528787')
+//       .then(channel => channel.messages.fetch('1476926871705878625'))
+//       .then(message => {
+//         setTimeout(()=> {
+//           return message.clickButton(map.meleeButtonId);
+//         },1000 * 2); //delay, sec
+//       })
+//       .then(() => console.log('Clicked melee'))
+//       .catch(console.error);
+// });
+
+client.login(process.env.TOKEN);
--- variables.js
+++ variables.js
@@ -1,5 +1,6 @@
 const map = {
     mentionStart: "<@&",
+    searchPosition: 21,
     bratosikChannelId : "1354880760917528787",
     gienaChannelId: "1236328199298748416",
     defaultChannel: "1279694692354756621",
@@ -9,12 +10,13 @@ const map = {
     svinRole: "1249407264838909952",
     raidRole: "1354876857060823273",
     newGienaRole: "1324082276337975306",
+    meleeButtonId: "raid_melee",
     usersId: ["379311012970758145", //bratosik
             "140706005754314752", //black
             "174560556374687744", //cez
-            "1429998990312603910", //v1
-            "1476377779833671700" //v2
-    ]
+            "1429998990312603910" //v1
+    ],
+    botId: "1476377779833671700"
 }
 
 module.exports = map;
